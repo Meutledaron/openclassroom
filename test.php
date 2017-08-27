3f1342906589b2b8c583bcbb6872c254c37ee7c7
Meutledaron <meutledaron@gmail.com>
2017-08-27 20:58:18 +0200

    Troisième commit : Ajout de commentaire dans le fichier 'test.php'

--- test.php
+++ test.php
@@ -1,10 +1,14 @@
 <?php
+//Création de l'array
 $alpha = array();
+//Première lettre de l'alphabet
 $start = "a";
+//Création de la boucle for pour ajouter toutes les lettres de l'alphabet dans l'array $alpha
 for($i=0; $i<26; $i++)
 {
 	$alpha[] = $start;
 	$start++;
 }
+//Affichage du message secret
 echo ucfirst($alpha[18].$alpha[0].$alpha[11].$alpha[20].$alpha[19]);
 ?>
